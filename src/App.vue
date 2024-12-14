a0b5e4747149c0252cc65ed337a2041bdf925952
Галина Слесарева <galaslesareva@gmail.com>
2024-12-14 04:42:07 +0300

    add Верстка: компоненты Заголовок, Подвал, Карточка товара

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrapper">
+    <herro-view />
+    <footer-component />
+  </div>
+</template>
+
+<script>
+import HerroView from "@/views/HerroView.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+
+export default {
+  components: { HerroView, FooterComponent },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
